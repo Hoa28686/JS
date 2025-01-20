3a14710a6a72583704b7843f140941f5b999378b
Hoa28686 <s2500149@edu.bc.fi>
2025-01-20 15:53:16 +0200

    rearange comments

--- week2/02_exercise/numbers/numbers_03.js
+++ week2/02_exercise/numbers/numbers_03.js
@@ -1,19 +1,23 @@
 /* 
-
 Create two variables:
 num1 with the value "10".
 num2 with the value "notANumber".
-Convert num1 and num2 using parseFloat().
-Print the sum of num1 and num2 using console.log().
-(Note: If one value is NaN, the result will be NaN, which will be printed automatically.)
-Example output:
-NaN (if the addition involves invalid input).
-10 (if both inputs are valid numbers).
-
 */
 
 let num1 = '10';
 let num2 ='notANumber';
+
+
+// Convert num1 and num2 using parseFloat().
+
 let num1_float = parseFloat(num1);
 let num2_float = parseFloat(num2);
+
+
+// Print the sum of num1 and num2 using console.log().
+// (Note: If one value is NaN, the result will be NaN, which will be printed automatically.)
+// Example output:
+// NaN (if the addition involves invalid input).
+// 10 (if both inputs are valid numbers).
+
 console.log('The sum of num1 and num2: ', num1_float + num2_float);
